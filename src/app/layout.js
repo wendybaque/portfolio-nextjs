b70d34a1daf4c6e40d5c285e7c102942dea7ee4f
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-09-08 14:03:10 +0200

    Update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,24 @@
-import './globals.css'
-import { Quicksand } from 'next/font/google'
-import Footer from '@/components/footer/Footer'
-import Navbar from '@/components/navbar/Navbar'
+import "./globals.css";
+import { Quicksand } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 
-const quicksand = Quicksand({ subsets: ['latin'] })
-
+const quicksand = Quicksand({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Wendy Baqué | Portfolio',
-  description: 'Portfolio de Wendy Baqué, développeuse web freelance et no code friendly !',
-}
+  title: "Wendy Baqué | Portfolio",
+  description:
+    "Portfolio de Wendy Baqué, développeuse web freelance et no code friendly !",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      LAYOUT
-      <Navbar />
-      <body className={quicksand.className}>{children}</body>
-      <Footer />
+      <body className={quicksand.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
