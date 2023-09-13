b8688ebadd2cf70985252a70ade5424af03313d0
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-09-13 10:34:57 +0200

    Add Hero style

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.infrastructureLogging = {
+            level: "error",
+        };
 
+        return config;
+    },
     images: {
         domains:[
             "images.pexels.com",
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,37 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Link from "next/link";
+import Wendev from "public/wendev.png";
 export default function Home() {
   return (
-    <main>
-      <div className={styles.texts}>
-        <h1>Wendy Baqué</h1>
-        <h2>Développeuse front-en en freelance et no code friendly !</h2>
-        <p>
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Wendy Baqué</h1>
+        <h2 className={styles.title2}>
+          Développeuse front-end en freelance et no code friendly !
+        </h2>
+        <p className={styles.desc}>
           💻 A la suite de ma formation à la Wild Code School et un stage très
           enrichissant chez Fedmind et de bénévolat chez O Media et Nelixair,
           j'ai créé WenDev, ma propre entreprise qui vous proposera des
           solutions numériques personnalisées, modernes et adaptées à vos
           besoins.
         </p>
+        <button className={styles.button}>
+          <Link href="/projects" alt="redirection vers la page des projets">
+            Voir mes projets
+          </Link>
+        </button>
       </div>
-      <div className={styles.imgContainer}></div>
-      <Image
-        src="/wendev.png"
-        width={100}
-        height={100}
-        alt="Wendy Baqué by Marwan Usman"
-        aria-label="Wendy Baqué by Marwan Usman"
-      />
+      <div className={styles.item}>
+        <Image
+          src={Wendev}
+          className={styles.img}
+          alt="Wendy Baqué by Marwan Usman"
+          aria-label="Wendy Baqué by Marwan Usman"
+        />
+      </div>
     </main>
   );
 }
