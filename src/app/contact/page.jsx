babf21d3ae22028a7a1931209fa0df9d179213c4
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-09-19 10:23:02 +0200

    Update contact page with Jotform

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,37 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
-"use client";
-import React, { useRef, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import emailjs from "@emailjs/browser";
+import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 import Fusée from "/public/contact.png";
+import Jotform from "@/components/jotform/Jotform";
 
 const Contact = () => {
-  const notify = () =>
-    toast("Merci pour votre message. Je vous répondrai au plus vite.");
-  const formRef = useRef();
-  const [done, setDone] = useState(false);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    emailjs
-      .sendForm(
-        "service_ijptha3",
-        "template_7xuccag",
-        formRef.current,
-        "-F7_Yn5YXZZOlGfoZ"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+ 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Contact</h1>
@@ -55,87 +30,8 @@ const Contact = () => {
         <form
           action=""
           className={styles.form}
-          ref={formRef}
-          onSubmit={handleSubmit}
         >
-          <label className={styles.label} htmlFor="name">
-            Votre nom
-            <input
-              type="text"
-              placeholder="Ex: Bouchard"
-              className={styles.input}
-              name="user_name"
-              required
-              aria-required="true"
-              autoComplete="on"
-              style={{ caretColor: "blue" }}
-            />
-          </label>
-
-          <label className={styles.label} htmlFor="phone">
-            Votre numéro de téléphone
-            <input
-              type="tel"
-              placeholder="ex: 0612345678"
-              name="user_phone"
-              required
-              aria-required="true"
-              autoComplete="on"
-              style={{ caretColor: "blue" }}
-              className={styles.input}
-            />
-          </label>
-
-          <label className={styles.label} htmlFor="email">
-            Votre adresse e-mail
-            <input
-              type="email"
-              placeholder="ex : exemple@gmail.com"
-              name="user_email"
-              required
-              aria-required="true"
-              autoComplete="on"
-              style={{ caretColor: "blue" }}
-              className={styles.input}
-            />
-          </label>
-
-          <label className={styles.label} htmlFor="subject">
-            Le sujet de votre message
-            <input
-              type="text"
-              placeholder="ex: J'ai une mission tech à vous proposer !"
-              name="user_subject"
-              required
-              aria-required="true"
-              autoComplete="off"
-              style={{ caretColor: "blue" }}
-              className={styles.input}
-            />
-          </label>
-
-          <label className={styles.label} htmlFor="message">
-            Votre message
-            <textarea
-              className={styles.textArea}
-              cols="30"
-              rows="10"
-              placeholder="Message"
-              name="message"
-              required
-              aria-required="true"
-              style={{ caretColor: "blue" }}
-            ></textarea>
-          </label>
-          <button
-            type="submit"
-            onClick={notify}
-            aria-pressed="false"
-            className={styles.button}
-          >
-            Envoyer
-          </button>
-          {done && <ToastContainer />}
+         <Jotform />
         </form>
       </div>
     </div>
---
+++ src/components/jotform/Jotform.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Jotform = () => {
+  return (
+    <div>
+      <iframe
+        id="JotFormIFrame-232611431637349"
+        title="Formulaire de contact portfolio"
+        onload="window.parent.scrollTo(0,0)"
+        allowtransparency="true"
+        allowfullscreen="true"
+        allow="geolocation; microphone; camera"
+        src="https://form.jotform.com/232611431637349"
+        frameborder="0"
+        style={{
+          minWidth: "100%",
+          height: "1200px",
+          border: "none",
+          borderRadius: "10px",
+        }}
+        scrolling="no"
+      >
+        {" "}
+      </iframe>
+    </div>
+  );
+};
+
+export default Jotform;
